6ab046e7d94927e4e3bc65fde13cb816b803a1a1
RAKAYADAV <rakayadav222139@gmail.com>
2024-05-22 22:56:01 +0530

    air-asia

---
+++ air-asia-project/src/main/java/com/ind/airasiaproject/dto/BookingDetails.java
@@ -0,0 +1,45 @@
+package com.ind.airasiaproject.dto;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingDetails {
+
+	@Id
+	private String pnr;
+	@CreationTimestamp
+	private LocalDate bookingDate;
+	private LocalDate journeyDate;
+	private String customerName;
+	private String cutomerEmail;
+	private long customerPhone;
+	private String passportNumber;
+	private double totalPrice;
+	@Column(name = "conveniencefee")
+	final private double CONVENIENCE_FEE = 300;
+	private String cancel = "Yes";
+
+	 @ManyToOne
+	 @JoinColumn(name = "userid")
+	private User user;
+	 
+	 @OneToOne
+	 @JoinColumn(name = "flightnumber")
+	 private Flight flight1;
+
+}
